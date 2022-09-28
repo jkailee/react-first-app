3cf4161c553f68bc54313edba89d182cbe6e03fe
Jeremiah <jlee814@calpoly.edu>
2022-09-28 10:38:06 -0700

    added queries

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -38,11 +38,23 @@ const users = {
        {
           id: 'zap555', 
           name: 'Dennis',
-          job: 'Bartender',
+          job: 'Althlete',
        }
     ]
  }
 
  app.get('/users', (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined){
+        let result = findUserByName(name);
+        result = {users_list: result};
+        res.send(result);
+    }
+    else{
+        res.send(users);
+    }
 });
+
+const findUserByName = (name) => { 
+    return users['users_list'].filter( (user) => user['name'] === name); 
+}
