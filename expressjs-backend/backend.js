5ebbbc73fd69a629615f90c3c5709f140ed5e89e
Jeremiah <jlee814@calpoly.edu>
2022-09-30 12:38:53 -0700

    added axios and CORS to enable resource sharing

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -2,7 +2,9 @@ const express = require('express');
 const { restart } = require('nodemon');
 const app = express();
 const port = 5000;
+const cors = require('cors');
 
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,6 +1,10 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
 import Table from './Table';
 import Form from './Form';
+
+
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
@@ -15,6 +19,24 @@ function MyApp() {
   function updateList(person) {
     setCharacters([...characters, person]);
   }
+
+  async function fetchAll(){
+    try {
+      const response = await axios.get('http://localhost:5000/users');
+      return response.data.users_list;     
+    }
+    catch (error){
+      //We're not handling errors. Just logging into the console.
+      console.log(error); 
+      return false;         
+    }
+  }
+  useEffect(() => {
+    fetchAll().then( result => {
+      if (result)
+        setCharacters(result);
+    });
+  }, [] );
 
   return (
     <div className="container">
